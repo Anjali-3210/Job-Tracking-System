db7118aac95a32d9713f24b8c735028cf7e928d8
Anju3210 <anjalipawar3210@gmail.com>
2026-07-16 18:26:28 +0530

    Day 2: Setup frontend and backend foundation

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

781f1d98ed0b056ae69939dda63c8c39da1a9ce7
Anju3210 <anjalipawar3210@gmail.com>
2026-07-17 18:00:21 +0530

    Day 4: Add user registration API with password hashing

--- server/index.js
+++ server/index.js
@@ -1,14 +1,52 @@
-require("dotenv").config();
+import "dotenv/config";
+import express from "express";
+import { PrismaClient } from "./generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+import bcrypt from "bcrypt";
 
-const express = require("express");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
+const prisma = new PrismaClient({ adapter });
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+
+    res.json({
+      message: "User registered successfully",
+      user: user,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

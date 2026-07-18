503280628f452ced3fd0f22af649f96a39f07b57
Anju3210 <anjalipawar3210@gmail.com>
2026-07-18 17:36:12 +0530

    Day 6: Implement JWT authentication and protected routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { PrismaClient } from "./generated/prisma/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,6 +20,26 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
+
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send("Access Denied. No Token Provided.");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).send("Invalid Token");
+  }
+}
 
 app.post("/register", async (req, res) => {
   try {
@@ -66,12 +87,30 @@ app.post("/login", async (req, res) => {
     return res.status(401).send("Invalid Password");
   }
 
+  const token = jwt.sign(
+  {
+    id: user.id,
+    email: user.email,
+  },
+  process.env.JWT_SECRET
+  );
+
+  console.log(token);
+
   const { password: _, ...userData } = user;
 
 res.status(200).json({
   message: "Login Successful",
+  token: token,
   user: userData,
 });
+});
+
+app.get("/profile", verifyToken, (req, res) => {
+  res.status(200).json({
+    message: "Welcome to your profile",
+    user: req.user,
+  });
 });
 
 app.listen(PORT, () => {

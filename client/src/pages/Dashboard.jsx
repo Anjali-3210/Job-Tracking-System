df6c9ed33f6b2ae66c47b3c05d5c5ddcaac5506a
Anju3210 <anjalipawar3210@gmail.com>
2026-07-27 11:57:41 +0530

    Day 14: Add job creation functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,9 @@ import api from "../services/api";
 const Dashboard = () => {
     const user = JSON.parse(localStorage.getItem("user"));
     const [jobs, setJobs] = useState([]);
+    const [company, setCompany] = useState("");
+    const [position, setPosition] = useState("");
+    const [status, setStatus] = useState("Applied");
     const navigate = useNavigate();
 
     const handleLogout = () => {
@@ -26,6 +29,28 @@ useEffect(() => {
   fetchJobs();
 }, []);
 
+const handleAddJob = async () => {
+  try {
+    await api.post("/jobs", {
+  company,
+  position,
+  status,
+});
+
+const response = await api.get("/jobs");
+setJobs(response.data);
+
+setCompany("");
+setPosition("");
+setStatus("Applied");
+
+alert("Job Added Successfully!");
+  } catch (error) {
+    console.log(error);
+    alert("Failed to add job");
+  }
+};
+
   return (
     <div>
       <h1>Dashboard</h1>
@@ -33,6 +58,42 @@ useEffect(() => {
       <button onClick={handleLogout}>
         Logout
       </button>
+
+      <h2>Add New Job</h2>
+
+      <input
+        type="text"
+        placeholder="Company"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Position"
+        value={position}
+        onChange={(e) => setPosition(e.target.value)}
+      />
+
+      <br /><br />
+
+      <select
+        value={status}
+        onChange={(e) => setStatus(e.target.value)}
+      >
+        <option value="Applied">Applied</option>
+        <option value="Interview">Interview</option>
+        <option value="Offer">Offer</option>
+        <option value="Rejected">Rejected</option>
+      </select>
+
+      <br /><br />
+
+      <button onClick={handleAddJob}>Add Job</button>
+
+      <hr />
 
       <h2>Your Jobs</h2>
 
